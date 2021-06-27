7134270d3e7162aeae8b921c9db85a7261eeded0
oskasf <oskasheldonfentem1999@outlook.com>
2021-06-27 20:05:16 +0000

    Test commit from bash script

--- bash/pull_effective.sh
+++ bash/pull_effective.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-# A script to pull data from effective charities and merge the new data
+# A script to pull data from effective charities
+
+# Testing git push from command line
+GIT=`which git`
+${GIT} add --all
+${GIT} commit -m "Test commit from bash script"
+${GIT} push
