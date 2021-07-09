014658fe6d916033d19c182539dcb977b6095b87
oskasf <oskasheldonfentem1999@outlook.com>
2021-07-09 16:51:14 +0000

    Data munge:

--- bash/data_munge.sh
+++ bash/data_munge.sh
@@ -3,3 +3,10 @@
 # A script to combine, clean and tidy data files
 
 Rscript R/process_data/main.R
+
+# Push to Github
+GIT=`which git` 
+${GIT} pull
+${GIT} add --all
+${GIT} commit -m "Data munge: `date + '%Y-%m-%d'`"
+${GIT} push
